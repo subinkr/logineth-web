1ede7df8d4104ac20f71e557290d47c3d4d11a1c
subin <github@subin.kr>
2024-01-06 13:56:29 +0900

    Profile: Redirect to about or profile

--- app/page.js
+++ app/page.js
@@ -3,12 +3,17 @@
 import { useEffect } from "react";
 import { useRecoilValue } from "recoil";
 import { profileState } from "@/components/recoil/profile";
+import callRedirect from "@/function/server/callRedirect";
 
 export default function Home() {
     const profile = useRecoilValue(profileState);
+    console.log(profile);
 
     useEffect(() => {
         if (profile.id) {
+            callRedirect(`/profile/${profile.id}`);
+        } else {
+            callRedirect("/about");
         }
     }, []);
 
