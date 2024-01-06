3399b2765d2220a4f2a833fed704c8cc3e1db900
subin <github@subin.kr>
2024-01-06 17:27:05 +0900

    Profile: Make userInfo

--- app/profile/[id]/userInfo.js
+++ app/profile/[id]/userInfo.js
@@ -1,23 +1,42 @@
+import Button from "@/components/button/button";
 import classes from "./userInfo.module.css";
 
 export default function UserInfo({ thisProfile, loginUser }) {
     return (
         <div className={classes.box}>
-            <img className={classes.image} src={thisProfile?.image} />
-            <div className={classes.info}>
-                <div>{thisProfile?.nickname}</div>
-                <div className={classes.created}>
-                    {thisProfile?.createdAt
-                        ? new Date(
-                              Date.parse(thisProfile?.createdAt)
-                          ).toLocaleDateString(undefined, {
-                              year: "numeric",
-                              month: "2-digit",
-                              day: "2-digit",
-                          })
-                        : "User not exist"}
+            <div className={classes.user}>
+                <div className={classes["image-wrapper"]}>
+                    <img className={classes.image} src={thisProfile?.image} />
+                </div>
+                <div className={classes.info}>
+                    <div>{thisProfile?.nickname}</div>
+                    <div className={classes.created}>
+                        {thisProfile?.createdAt
+                            ? new Date(
+                                  Date.parse(thisProfile?.createdAt)
+                              ).toLocaleDateString(undefined, {
+                                  year: "numeric",
+                                  month: "2-digit",
+                                  day: "2-digit",
+                              })
+                            : "User not exist"}
+                    </div>
                 </div>
             </div>
+            {thisProfile?.id ? (
+                thisProfile?.id === loginUser?.id ? (
+                    <div className={classes.buttons}>
+                        <Button>Edit</Button>
+                        <Button type="danger">Withdraw</Button>
+                    </div>
+                ) : (
+                    <>
+                        <Button>Follow</Button>
+                    </>
+                )
+            ) : (
+                <></>
+            )}
         </div>
     );
 }
