87ce56f2acb42b71293986c27c19d06b3eba761c
subin <github@subin.kr>
2024-01-08 16:19:29 +0900

    Profile: Make saveEdit

--- app/profile/[id]/userInfo.js
+++ app/profile/[id]/userInfo.js
@@ -4,12 +4,15 @@ import Button from "@/components/button/button";
 import classes from "./userInfo.module.css";
 import getCookie from "@/function/server/getCookie";
 import deleteCookie from "@/function/server/deleteCookie";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Input from "@/components/input/input";
 
 export default function UserInfo({ profile, loginUser }) {
     const [edit, setEdit] = useState(false);
+    const [image, setImage] = useState("");
     const [nickname, setNickname] = useState("");
+    const [bio, setBio] = useState("");
+    const inputRef = useRef(null);
 
     const withdraw = async () => {
         if (confirm("탈퇴하시겠습니까? 데이터는 복구할 수 없습니다.")) {
@@ -29,80 +32,124 @@ export default function UserInfo({ profile, loginUser }) {
         }
     };
 
-    const saveEdit = async () => {};
+    const saveEdit = async () => {
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_SERVER}/profile/${profile.id}/edit`,
+            {
+                method: "put",
+                headers: {
+                    Authorization: `Bearer ${await getCookie()}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    image,
+                    nickname,
+                    bio,
+                }),
+            }
+        );
+        const result = await response.json();
+        setEdit(false);
+
+        alert(result.message);
+    };
+
+    const fileSelect = () => {
+        // TODO: Image upload
+    };
+
+    const editImage = () => {
+        inputRef.current.click();
+    };
+
+    const editNickname = (e) => {
+        setNickname(e.target.value);
+    };
+
+    useEffect(() => {
+        if (edit) {
+            setImage(image ? image : profile?.image);
+            setNickname(nickname ? nickname : profile?.nickname);
+        }
+    }, [edit]);
 
     return (
-        <div className={classes.box}>
-            <div className={classes.user}>
-                <div className={classes["image-wrapper"]}>
-                    {edit ? (
-                        <img
-                            className={classes["image-wrapper"]}
-                            src={profile?.image}
-                        />
-                    ) : (
-                        <img className={classes.image} src={profile?.image} />
-                    )}
-                </div>
-                <div className={classes.info}>
-                    {edit ? (
-                        <Input
-                            value={nickname}
-                            onChange={() => setNickname(nickname)}
-                        />
-                    ) : (
-                        <div>{profile?.nickname}</div>
-                    )}
-                    <div className={classes.created}>
-                        {profile?.createdAt
-                            ? new Date(
-                                  Date.parse(profile?.createdAt)
-                              ).toLocaleDateString(undefined, {
-                                  year: "numeric",
-                                  month: "2-digit",
-                                  day: "2-digit",
-                              })
-                            : "User not exist"}
+        <>
+            <div className={classes.box}>
+                <div className={classes.user}>
+                    <div className={classes["image-wrapper"]}>
+                        {edit ? (
+                            <>
+                                <img
+                                    className={classes["image-wrapper"]}
+                                    src={image}
+                                    onClick={editImage}
+                                    style={{ cursor: "pointer" }}
+                                />
+                            </>
+                        ) : (
+                            <img
+                                className={classes.image}
+                                src={image ? image : profile?.image}
+                            />
+                        )}
+                    </div>
+                    <div className={classes.info}>
+                        {edit ? (
+                            <Input value={nickname} onChange={editNickname} />
+                        ) : (
+                            <div>{nickname ? nickname : profile?.nickname}</div>
+                        )}
+                        <div className={classes.created}>
+                            {profile?.createdAt
+                                ? new Date(
+                                      Date.parse(profile?.createdAt)
+                                  ).toLocaleDateString(undefined, {
+                                      year: "numeric",
+                                      month: "2-digit",
+                                      day: "2-digit",
+                                  })
+                                : "User not exist"}
+                        </div>
                     </div>
                 </div>
-            </div>
-            {profile?.id ? (
-                profile?.id === loginUser?.id ? (
-                    edit ? (
-                        <div className={classes.buttons}>
-                            <Button
-                                onClick={() => {
-                                    setEdit(!edit);
-                                }}
-                            >
-                                Cancel
-                            </Button>
-                            <Button type="primary" onClick={saveEdit}>
-                                Save
-                            </Button>
-                        </div>
+                {profile?.id ? (
+                    profile?.id === loginUser?.id ? (
+                        edit ? (
+                            <div className={classes.buttons}>
+                                <Button onClick={() => setEdit(false)}>
+                                    Cancel
+                                </Button>
+                                <Button type="primary" onClick={saveEdit}>
+                                    Save
+                                </Button>
+                            </div>
+                        ) : (
+                            <div className={classes.buttons}>
+                                <Button onClick={() => setEdit(true)}>
+                                    Edit
+                                </Button>
+                                <Button type="danger" onClick={withdraw}>
+                                    Withdraw
+                                </Button>
+                            </div>
+                        )
                     ) : (
-                        <div className={classes.buttons}>
-                            <Button
-                                onClick={() => {
-                                    setEdit(!edit);
-                                }}
-                            >
-                                Edit
-                            </Button>
-                            <Button type="danger" onClick={withdraw}>
-                                Withdraw
-                            </Button>
-                        </div>
+                        <>
+                            <Button>Follow</Button>
+                        </>
                     )
                 ) : (
-                    <>
-                        <Button>Follow</Button>
-                    </>
-                )
-            ) : (
-                <></>
-            )}
-        </div>
+                    <></>
+                )}
+            </div>
+            <input
+                type="file"
+                accept="image/*"
+                hidden
+                ref={inputRef}
+                onChange={fileSelect}
+            />
+        </>
     );
 }
--- components/input/input.js
+++ components/input/input.js
@@ -1,7 +1,5 @@
 import classes from "./input.module.css";
 
-export default function Input({ value, onChange }) {
-    return (
-        <input className={classes.input} value={value} onChange={onChange} />
-    );
+export default function Input(props) {
+    return <input className={classes.input} {...props} />;
 }
