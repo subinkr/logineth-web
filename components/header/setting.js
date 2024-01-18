b6d61d2027d51c5df6d06bccb02cc40bcddf3108
subin <github@subin.kr>
2024-01-18 13:11:13 +0900

    Fix: Bug

--- components/header/setting.js
+++ components/header/setting.js
@@ -3,34 +3,44 @@
 import classes from "./setting.module.css";
 import { useEffect, useState } from "react";
 import Language from "./language";
+import { useRecoilState } from "recoil";
+import { profileState } from "../recoil/profile";
 
 export default function Setting() {
+    const [loginUser, setLoginUser] = useRecoilState(profileState);
     const [setting, setSetting] = useState(true);
+    const [language, setLanguage] = useState("");
 
     useEffect(() => {
-        setSetting(!setting);
+        setSetting(false);
     }, []);
+
+    useEffect(() => {
+        setLanguage(loginUser.language);
+        if (setting && language === loginUser.language) {
+            setSetting(false);
+        }
+        if (!setting) {
+            setSetting(true);
+            setLoginUser({ ...loginUser });
+        }
+    }, [loginUser]);
 
     return (
         <div>
             {setting ? (
                 <>
                     <Language />
-                    <button
-                        className={classes.setting}
-                        onClick={() => setSetting(!setting)}
-                    >
-                        ⚙️
-                    </button>
                 </>
             ) : (
-                <button
-                    className={classes.setting}
-                    onClick={() => setSetting(!setting)}
-                >
-                    ⚙️
-                </button>
+                <></>
             )}
+            <button
+                className={classes.setting}
+                onClick={() => setSetting(!setting)}
+            >
+                ⚙️
+            </button>
         </div>
     );
 }
