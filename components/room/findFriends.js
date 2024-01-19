cf6488495b5008cb2aeab98b51faf7cfe97c5cab
subin <github@subin.kr>
2024-01-19 13:59:32 +0900

    Room: Make search in findFriends

--- components/room/findFriends.js
+++ components/room/findFriends.js
@@ -4,9 +4,26 @@ import { useRecoilValue } from "recoil";
 import classes from "./findFriends.module.css";
 import { languageState } from "../recoil/language";
 import Button from "../button/button";
+import { useEffect, useRef } from "react";
 
 export default function FindFriends({ setFindFriends }) {
     const language = useRecoilValue(languageState);
+    const inputRef = useRef();
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []);
+
+    const search = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_SERVER}/friend/find/1?keyword=${inputRef.current.value}`
+        );
+        const result = await response.json();
+        console.log(result);
+        inputRef.current.value = "";
+    };
 
     const closeFindFriends = () => {
         setFindFriends(false);
@@ -14,7 +31,14 @@ export default function FindFriends({ setFindFriends }) {
 
     return (
         <>
-            <div className={classes.room}></div>
+            <div className={classes.room}>
+                <form>
+                    <input ref={inputRef} className={classes.header} />
+                    <Button className="search" type="submit" onClick={search}>
+                        🔍
+                    </Button>
+                </form>
+            </div>
             <Button className={"default"} onClick={closeFindFriends}>
                 {language?.close}
             </Button>
