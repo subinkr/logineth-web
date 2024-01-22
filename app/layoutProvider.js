415f68851bc22ed738373fff6df34bef4a4bef2e
subin <github@subin.kr>
2024-01-22 17:53:16 +0900

    Test: UI test

--- app/layoutProvider.js
+++ app/layoutProvider.js
@@ -13,8 +13,8 @@ import UseRecoil from "@/function/client/useRecoil";
 export default function LayoutProvider({ cookie, children }) {
     const pathname = usePathname();
     const layoutRef = useRef();
-    const [height, setHeight] = useState(null);
-    const [windowHeight, setWindowHeight] = useState(null);
+    const [height, setHeight] = useState(0);
+    const [windowHeight, setWindowHeight] = useState(0);
 
     const callback = () => {
         if (layoutRef.current?.offsetWidth > 768) {
@@ -55,20 +55,24 @@ export default function LayoutProvider({ cookie, children }) {
             className={classes.layout}
             ref={layoutRef}
             style={
-                windowHeight < height && {
-                    height: height,
-                    overflow: "hidden",
-                    touchAction: "none",
-                }
+                windowHeight < height
+                    ? {
+                          height: height,
+                          overflow: "hidden",
+                          touchAction: "none",
+                      }
+                    : {}
             }
         >
             <body
                 style={
-                    windowHeight < height && {
-                        height: height,
-                        overflow: "hidden",
-                        touchAction: "none",
-                    }
+                    windowHeight < height
+                        ? {
+                              height: height,
+                              overflow: "hidden",
+                              touchAction: "none",
+                          }
+                        : {}
                 }
             >
                 <UseRecoil>
@@ -78,11 +82,11 @@ export default function LayoutProvider({ cookie, children }) {
                             <div
                                 className={classes.children}
                                 style={
-                                    windowHeight < height && {
-                                        height: height,
-                                        overflow: "hidden",
-                                        touchAction: "none",
-                                    }
+                                    windowHeight < height
+                                        ? {
+                                              touchAction: "none",
+                                          }
+                                        : {}
                                 }
                             >
                                 {children}
